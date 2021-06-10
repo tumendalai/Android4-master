1d35b4ff6eb37a75535744a1e9bb33d3a32ca233
Youdono <64512092+tumendalai@users.noreply.github.com>
2021-06-10 22:58:08 +0800

    Initial commit

---
+++ app/src/main/java/com/example/android4/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.android4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn=findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(i);
+            }
+        });
+    }
+}
